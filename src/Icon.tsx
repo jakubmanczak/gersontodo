0cc664dfd613cde9ffc9502fec09f74d2a1e5754
jakubmanczak <jakub@manczak.net>
2023-03-22 21:54:19 +0100

    first pass

---
+++ src/Icon.tsx
@@ -0,0 +1,84 @@
+type iconselection = "checked" | "unchecked" | "arrow-right" | "delete";
+
+type iconprops = {
+	icon: iconselection;
+} & React.SVGProps<SVGSVGElement>;
+
+const Icon = ({ icon, ...props }: iconprops) => {
+	switch (icon) {
+		case "checked":
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					className="feather feather-check-square"
+				>
+					<polyline points="9 11 12 14 22 4" />
+					<path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11" />
+				</svg>
+			);
+		case "unchecked":
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					className="feather feather-square"
+				>
+					<rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+				</svg>
+			);
+		case "arrow-right":
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					className="feather feather-arrow-right"
+				>
+					<line x1="5" y1="12" x2="19" y2="12" />
+					<polyline points="12 5 19 12 12 19" />
+				</svg>
+			);
+		case "delete":
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					className="feather feather-x-square"
+				>
+					<rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+					<line x1="9" y1="9" x2="15" y2="15" />
+					<line x1="15" y1="9" x2="9" y2="15" />
+				</svg>
+			);
+	}
+};
+
+export { Icon };
---
+++ src/Index.tsx
@@ -0,0 +1,181 @@
+import { useRef, useState } from "react";
+// żeby nie było że nie ma komponentów:
+// tutaj komponent z ikonkami.
+// nie widziałem sensu rozdzielać projektu na drobne
+// w przypadku tego projektu..
+import { Icon } from "./Icon";
+import "./Index.css";
+
+type task = {
+	content: string;
+	complete: boolean;
+};
+
+const defaultTasks: task[] = [
+	{
+		content: "do ur mum",
+		complete: true,
+	},
+	{
+		content: "do ur dad",
+		complete: true,
+	},
+	{
+		content: "do the laundry",
+		complete: false,
+	},
+];
+
+const Index = () => {
+	const [list, setList] = useState<task[]>(defaultTasks);
+	const [filter, setFilter] = useState<boolean>(false);
+	const [messageIndex, setMessageIndex] = useState<number>(0);
+	const inputRef = useRef<HTMLInputElement>(null);
+	return (
+		<>
+			<header>
+				<h1>to-do list</h1>
+				<p>
+					by
+					<a href="https://manczak.net" target="_blank">
+						@jakubmanczak
+					</a>
+				</p>
+				<p className="muted subtitle">
+					Written as a school project. <a href="/">Reset.</a>
+				</p>
+			</header>
+			<main>
+				<div className="controls">
+					<div className="newtask">
+						{/* 
+									ok, może dublowanie kodu złe,
+									ale nie chce mi się rozwiązywać tego lepiej
+
+									to nie enterprise :))
+
+									(( gdyby był też by mi się nie chciało ))
+						*/}
+						<input
+							type="text"
+							ref={inputRef}
+							onKeyDown={(event: React.KeyboardEvent) => {
+								if (event.key === "Enter") {
+									if (inputRef.current?.value == "") {
+										setMessageIndex(1);
+										return;
+									}
+									setList([
+										...list,
+										{
+											content: inputRef.current?.value || "",
+											complete: false,
+										},
+									]);
+									if (inputRef.current) inputRef.current.value = "";
+									setMessageIndex(0);
+								}
+							}}
+						/>
+						<button
+							onClick={() => {
+								if (inputRef.current?.value == "") {
+									setMessageIndex(1);
+									return;
+								}
+								if (inputRef.current)
+									setList([
+										...list,
+										{
+											content: inputRef.current?.value || "",
+											complete: false,
+										},
+									]);
+								if (inputRef.current) inputRef.current.value = "";
+								setMessageIndex(0);
+							}}
+						>
+							<Icon icon="arrow-right" />
+						</button>
+					</div>
+					<div className="filter">
+						<button
+							onClick={() => {
+								setFilter(!filter);
+							}}
+						>
+							{filter ? <Icon icon="checked" /> : <Icon icon="unchecked" />}
+						</button>
+						<p
+							onClick={() => {
+								setFilter(!filter);
+							}}
+						>
+							hide completed?
+						</p>
+					</div>
+				</div>
+				<p className={messageIndex === 1 ? "red" : "muted"}>
+					{messageIndex === 1
+						? "Najpierw coś napisz!"
+						: "- - - - - - - - - - - - - -"}
+				</p>
+				{list
+					.filter((el: task) => {
+						return filter ? !el.complete : true;
+					})
+					.map((el: task, index) => {
+						return (
+							<article
+								key={`${el.content}${index}`}
+								className={el.complete ? "complete" : ""}
+							>
+								<p>{el.content}</p>
+								<button
+									className="check"
+									onClick={() => {
+										setList(
+											list.map((inEl, inIndex) => {
+												return {
+													content: inEl.content,
+													complete:
+														index === inIndex ? !inEl.complete : inEl.complete,
+												};
+											})
+										);
+									}}
+								>
+									{el.complete ? (
+										<Icon icon="checked" />
+									) : (
+										<Icon icon="unchecked" />
+									)}
+								</button>
+								<button
+									className="remove"
+									onClick={() => {
+										setList(
+											list.filter((inEl, inIndex) => {
+												return index !== inIndex;
+											})
+										);
+									}}
+								>
+									<Icon icon="delete" />
+								</button>
+							</article>
+						);
+					})}
+				<div className="muted">
+					{list.filter((el: task) => {
+						return filter ? !el.complete : true;
+					}).length === 0
+						? "Ludzie, tu nikogo nie ma!"
+						: ""}
+				</div>
+			</main>
+		</>
+	);
+};
+
+export { Index };
